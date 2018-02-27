914be2ea2da519d2c2952ba784a754944da82900
Bernd Weckx <bernd.weckx@gmail.com>
2018-02-27 13:09:33 +0100

    Finishing touches on arguments, function descriptions and basic getters and setters.

--- OGP1718-Worms/src/worms/facade/Facade.java
+++ OGP1718-Worms/src/worms/facade/Facade.java
@@ -3,30 +3,34 @@ package worms.facade;
 import worms.model.Worm;
 import worms.util.ModelException;
 
-public class Facade implements IFacade{
+public class Facade implements IFacade {
+
+	// Default constructor for FACADE class
+	public Facade() {
+	}
 
 	@Override
 	public Worm createWorm(double[] location, double direction, double radius, String name) throws ModelException {
-		// TODO Auto-generated method stub
-		return null;
+		Worm w = new Worm(location, direction, radius, name);
+		return w;
 	}
 
 	@Override
 	public void move(Worm worm, int nbSteps) throws ModelException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void turn(Worm worm, double angle) throws ModelException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void jump(Worm worm) throws ModelException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -43,68 +47,57 @@ public class Facade implements IFacade{
 
 	@Override
 	public double getX(Worm worm) throws ModelException {
-		// TODO Auto-generated method stub
-		return 0;
+		return worm.getLocation()[0];
 	}
 
 	@Override
 	public double getY(Worm worm) throws ModelException {
-		// TODO Auto-generated method stub
-		return 0;
+		return worm.getLocation()[1];
 	}
 
 	@Override
 	public double getOrientation(Worm worm) throws ModelException {
-		// TODO Auto-generated method stub
-		return 0;
+		return worm.getRadius();
 	}
 
 	@Override
 	public double getRadius(Worm worm) throws ModelException {
-		// TODO Auto-generated method stub
-		return 0;
+		return worm.getRadius();
 	}
 
 	@Override
 	public void setRadius(Worm worm, double newRadius) throws ModelException {
-		// TODO Auto-generated method stub
-		
+		worm.setRadius(newRadius);
 	}
 
 	@Override
 	public long getNbActionPoints(Worm worm) throws ModelException {
-		// TODO Auto-generated method stub
-		return 0;
+		return worm.getCurrentActionPoints();
 	}
 
 	@Override
 	public void decreaseNbActionPoints(Worm worm, long delta) throws ModelException {
-		// TODO Auto-generated method stub
-		
+		worm.setActionPoints(worm.getCurrentActionPoints() - (int)Math.ceil(delta));
 	}
 
 	@Override
 	public long getMaxNbActionPoints(Worm worm) throws ModelException {
-		// TODO Auto-generated method stub
-		return 0;
+		return worm.getMaxActionPoints();
 	}
 
 	@Override
 	public String getName(Worm worm) throws ModelException {
-		// TODO Auto-generated method stub
-		return null;
+		return worm.getName();
 	}
 
 	@Override
 	public void rename(Worm worm, String newName) throws ModelException {
-		// TODO Auto-generated method stub
-		
+		worm.setName(newName);
 	}
 
 	@Override
 	public double getMass(Worm worm) throws ModelException {
-		// TODO Auto-generated method stub
-		return 0;
+		return worm.getMass();
 	}
 
 }
--- OGP1718-Worms/src/worms/model/Worm.java
+++ OGP1718-Worms/src/worms/model/Worm.java
@@ -1,30 +1,37 @@
 package worms.model;
-
-import java.lang.annotation.Annotation;
-
 import be.kuleuven.cs.som.annotate.*;
-import worms.exceptions.InvalidLocationException;
 import worms.util.ModelException;
 
 /**
- * A class describing our in-game characters, the worms. TODO
+ * A class describing our in-game characters, the worms.
  * 
  * @author Liam, Bernd
  * TODO CODE 
- * @Invar worm's location is whithin bounds.
- * 			|
- * @Invar worm's direction is whithin the bounds of 0 and 2Pi.
- * 			|
+ * @Invar worm's location is within bounds.
+ * 			| isValidLocation(getLocation())
+ * @Invar worm's direction is within the bounds of 0 and 2Pi.
+ * 			| getDirection() >= 0 && getDirection() < math.PI * 2
+ * @Invar worm's radius must be valid, larger than the minimum radius
+ * 			| isValidRadius(getRadius())
+ * @Invar worm's action points may never be less than 0 or larger than the worm's max AP
+ * 			| isValidAmountOfActionPoints(getCurrentActionPoints())
+ * @Invar ??? worm's mass and it's max action points  
+ * @Invar worm's name must be valid
+ * 			| nameContainsValidCharactersOnly(getName())
  */			
 public class Worm {
 	
-	//TODO
-	public Worm() {
-		
+	@Raw
+	public Worm(double[] location, double direction, double radius, String name) throws ModelException{
+		setLocation(location);
+		setDirection(direction);
+		setRadius(radius);
+		setName(name);
+		resetActionPoints();
 	}
-	
-	//Done
+
 	/**
+	 * Sets the location of a worm.
 	 * 
 	 * @param location
 	 * 
@@ -45,6 +52,7 @@ public class Worm {
 	}
 	
 	/**
+	 * Checks whether a given location exists and only contains valid coordinates.
 	 * 
 	 * @param location
 	 * @return True if the location is valid(not null) and both indexes are within the given bounds.
@@ -63,7 +71,7 @@ public class Worm {
 	private double[] location = new double[2];
 	
 	
-	/*
+	/**
 	 * returns the worm's current x and y location.
 	 */
 	@Basic @Raw
@@ -72,13 +80,17 @@ public class Worm {
 	}
 	
 	/**
+	 * Sets the direction that the worm should be facing. Direction is presented in radians from 0..2 * PI the latter not included.
+	 * 0 is facing right, PI / 4 is facing up, PI / 2 is facing left, PI = 0 is facing right again.
 	 * 
 	 * @param direction
+	 * 		The given direction the worm should be facing
 	 * @pre direction has to be bigger than 0 or equal to 0 and smaller than 2PI
 	 * 		|direction >= 0 && direction < 2*Math.pi
 	 * @post given direction equals worm's direction
 	 * 		|new.getDirection() == direction
 	 */
+	@Raw
 	public void setDirection(double direction) {
 		assert direction >= 0 && direction <2*Math.PI;
 		this.direction = direction;
@@ -86,7 +98,7 @@ public class Worm {
 	
 	private double direction;
 	
-	/*
+	/**
 	 * getDirection returns the worm its direction. 
 	 */
 	@Basic @Raw
@@ -94,7 +106,7 @@ public class Worm {
 		return this.direction;
 	}
 	
-	/*
+	/**
 	 * Returns the effective radius of the current worm.
 	 */
 	@Basic @Raw
@@ -102,25 +114,34 @@ public class Worm {
 		return this.radius;
 	}
 	
-	/* 
+	/**
+	 * Sets the radius of a worm.
+	 * 
 	 * @param radius
+	 * 		The given radius for a worm, bigger radius is bigger worm. Minimum radius may be in effect.
 	 * @post Sets the worm its radius to the given radius. 
 	 * 		|new.getRadius() == radius
+	 * @effect This function also changes the mass and thus max Action Points, since a worm's weight depends on it's size.
+	 * 		| setMass()
 	 * @throws IllegalArgumentException whenever the radius is out of bounds.
 	 *  	|!isValidRadius(radius)
 	 */
+	@Raw
 	public void setRadius(double radius) throws ModelException{
 		isValidRadius(radius); // crashes if its not true. So no further code needed. 
 		this.radius = radius;
+		setMass();
 	}
 	
 	private double radius = -1;
 	private final static double minRadius = 0.25;
 	
 	
-	/*
-	 * @param radius
+	/**
+	 * Checks whether a given radius is valid.
 	 * 
+	 * @param radius
+	 * 			The given radius for a worm, bigger radius is bigger worm. Minimum radius may be in effect.
 	 * @return True if the given radius is equal to or bigger than the minimum radius.
 	 * 			| result == 
 	 * 			| minRadius <= radius
@@ -128,61 +149,122 @@ public class Worm {
 	 *			| minRadius > radius
 	 */
 	public boolean isValidRadius(double radius) throws ModelException{
-		if  (this.minRadius > radius)
+		if  (minRadius > radius)
 			throw new ModelException("Radius out of bounds.");
-		return this.minRadius <= radius;
+		return minRadius <= radius;
 	}
 	
 	private static final double worm_density = 1062;
 	
-	/*
+	/**
+	 * Calculates and returns the resulting mass of a worm with it's properties (radius for example).
+	 * 
 	 * @post calculateMass returns the worms effective mass, calculated using its own radius and density.
 	 * 		|new.getMass() == worm_density * (((double)4 / (double)3)  * Math.PI * Math.pow(this.radius, 3))
 	 * @effect?????
 	 */
 	@Raw
 	public double calculateMass() {
-		return this.worm_density * (((double)4 / (double)3)  * Math.PI * Math.pow(this.radius, 3));
+		return worm_density * (((double)4 / (double)3)  * Math.PI * Math.pow(this.radius, 3));
 	}
 	
-	/*
+	/**
 	 * Returns the mass for the given worm. 
 	 */
 	@Basic
 	public double getMass() {
 		return this.mass;
 	}
-	/*
-	 * @post The worm its current mass eaquals the the newly calculated mass.
+	
+	/**
+	 * This function sets the theoretical mass of a worm. Since a worm's maximum Action Points depends on a worm's weight
+	 * the worm's max AP will be calculated and set in this function as well. 
+	 * 
+	 * @post The worm its current mass equals the the newly calculated mass.
 	 * 			|new.getMass() == this.calculateMass();
 	 * @post Whenever the worm its mass gets changed, also will its maxActionPoints
 	 * 			|new.getMaxActionPoints() = this.getMaxActionPoints();
 	 */
 	public void setMass(){
 		this.mass = calculateMass();
-		this.maxActionPoints = this.getMaxActionPoints();
+		this.maxActionPoints = this.calculateMaxActionPoints();
 	}
 	
 	private double mass;
 	
-	/*
+	/**
 	 * @return returns the maximum amount of AP for a worm with given mass.
 	 */
-	public int getMaxActionPoints() {
+	public int calculateMaxActionPoints() {
 		return Math.round((float)this.getMass());
 	}
 	
-	/*
+	/**
+	 * Returns the maximum amount of Action Points assigned to the worm
+	 * 
+	 * @return returns the maximum amount of AP assigned to the worm.
+	 */
+	@Basic
+	public int getMaxActionPoints() {
+		return this.maxActionPoints;
+	}
+	
+	/**
+	 * Resets the worm's active Action Points to the worm's maximum amount of AP.
+	 * 
 	 * @post Each time function is called, action points get reset tot their maximum value.
 	 * 			|new.getActonPoints() = this.maxActionPoints
 	 */
+	@Raw
 	public void resetActionPoints() {
 		this.currentActionPoints = this.maxActionPoints;
 	}
 	
-	/*
+	/**
+	 * @param amount the new amount a worm's action points should be set to
+	 * 
+	 * @post If a given amount of action points is possible for the worm, then the new AP amount will be assigned to the worm.
+	 * 		| if(isValidAmountOfActionPoints(amount))
+	 * 		|	then new.getCurrentActionPoints() == amount
+	 * 
+	 * @post If a given amount of action points is not possible for the worm, if the amount is greater than the max AP for the worm it's AP will be set to max.
+	 * 		| if(!isValidAmountOfActionPoints(amount) && amount > this.getMaxActionPoints() )
+	 * 		|	then new.getCurrentActionPoints() == this.getMaxActionPoints()
+	 * 
+	 * @post If a given amount of action points is not possible for the worm, if the amount is less than 0 it's AP will be set to 0.
+	 * 		| if(!isValidAmountOfActionPoints(amount) && amount < 0 )
+	 * 		|	then new.getCurrentActionPoints() == 0
+	 * 
+	 */
+	@Raw
+	public void setActionPoints(int amount) {
+		if(isValidAmountOfActionPoints(amount)) {
+			this.currentActionPoints = amount;
+		}else if(amount > this.getMaxActionPoints()) {
+			this.currentActionPoints = this.getMaxActionPoints();
+		}else if(amount < 0) {
+			this.currentActionPoints = 0;
+		}
+	}
+	
+	/**
+	 * Checks whether a given amount of action points is a valid amount for a certain worm.
+	 * This should be either greater or equal then zero and lower or equal to the worm's max AP.
+	 * 
+	 * @param amount
+	 * 			A given amount of Action Points
+	 * @return Is the given amount of AP possible for the worm
+	 * 			| result ==
+	 * 			| 	amount >= 0 && amount <= this.maxActionPoints;
+	 */
+	public boolean isValidAmountOfActionPoints(int amount) {
+		return amount >= 0 && amount <= this.maxActionPoints;
+	}
+	
+	/**
 	 * Returns the currentActionPoints for the current worm. 
 	 */
+	@Raw
 	public int getCurrentActionPoints() {
 		return currentActionPoints;
 	}
@@ -191,14 +273,17 @@ public class Worm {
 	private int maxActionPoints;
 	
 	
-	/*
+	/**
+	 * Sets the name of the worm.
+	 * 
 	 * @post Given name becomes the worm its name.
 	 * 			|new.getName() = name;
 	 * @throws IllegalArgumentException whenever character contains an invalid character.
 	 * 			| !isValidName(name)
 	 */
+	@Raw
 	public void setName(String name) throws ModelException{
-		if (!NameContainsValidCharactersOnly(name))
+		if (!nameContainsValidCharactersOnly(name))
 			throw new ModelException("Please enter a name containing only valid characters."); // DO WE KEEP THIS OR DO WE REWRITE THIS LIKE setRadius???
 		this.name = name;
 		return;
@@ -206,8 +291,8 @@ public class Worm {
 	
 	private String name;
 	
-	/*
-	 * @return returns true if given character is a letter
+	/**
+	 * @return returns true if given character is a valid character
 	 * 
 	 */
 	public static boolean isValidCharacter(char c) {
@@ -220,7 +305,9 @@ public class Worm {
 		return false;
 	}
 	
-	/*
+	/**
+	 * Check whether a string name only contains characters that are allowed by the programmer.
+	 * 
 	 * @return True if all characters in string name are valid.
 	 * 			|for (char c : name.toCharArray())
 	 *			|	isValidCharacter(c)
@@ -230,7 +317,7 @@ public class Worm {
 	 * 			|for (char c : name.toCharArray())
 				|	!isValidCharacter(c)
 	 */
-	public static boolean NameContainsValidCharactersOnly(String name) throws ModelException{
+	public static boolean nameContainsValidCharactersOnly(String name) throws ModelException{
 		if (name == null)
 			throw new ModelException("Please enter a name containing only valid characters.");
 		for (char c : name.toCharArray()) {
@@ -240,10 +327,10 @@ public class Worm {
 		return true;
 	}
 	
-	/*
-	 * returns the given name
+	/**
+	 * returns the given name of the worm.
 	 */
-	@Basic
+	@Basic @Raw
 	public String getName() {
 		return name;
 	}
