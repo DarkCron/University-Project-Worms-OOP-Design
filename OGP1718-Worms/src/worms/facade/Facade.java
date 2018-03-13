f2869dce6af2a0c0db264dd6163f58002cba397d
Bernd Weckx <bernd.weckx@gmail.com>
2018-03-13 12:28:08 +0100

    Preparations part II

--- OGP1718-Worms/src/worms/facade/Facade.java
+++ OGP1718-Worms/src/worms/facade/Facade.java
@@ -1,8 +1,17 @@
 package worms.facade;
 
+import java.math.BigInteger;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
 import worms.exceptions.InvalidLocationException;
 import worms.exceptions.InvalidRadiusException;
 import worms.exceptions.InvalidWormNameException;
+import worms.model.Food;
+import worms.model.Team;
+import worms.model.World;
+import worms.model.WorldConstants;
 import worms.model.Worm;
 import worms.util.ModelException;
 
@@ -12,31 +21,31 @@ public class Facade implements IFacade {
 	public Facade() {
 	}
 
-	@Override
-	public Worm createWorm(double[] location, double direction, double radius, String name) throws ModelException {
-		Worm w = null;
-		
-		try {
-			w = new Worm(location, direction, radius, name);
-		} catch (InvalidLocationException e) {
-			throw new ModelException(e);
-		} catch (InvalidWormNameException e) {
-			throw new ModelException(e);
-		} catch (InvalidRadiusException e) {
-			throw new ModelException(e);
-		}
-		
-		return w;
-	}
+//	@Override
+//	public Worm createWorm(double[] location, double direction, double radius, String name) throws ModelException {
+//		Worm w = null;
+//		
+//		try {
+//			w = new Worm(location, direction, radius, name);
+//		} catch (InvalidLocationException e) {
+//			throw new ModelException(e);
+//		} catch (InvalidWormNameException e) {
+//			throw new ModelException(e);
+//		} catch (InvalidRadiusException e) {
+//			throw new ModelException(e);
+//		}
+//		
+//		return w;
+//	}
 
-	@Override
-	public void move(Worm worm, int nbSteps) throws ModelException {
-		try {
-			worm.Move(nbSteps);
-		} catch (InvalidLocationException e) {
-			throw new ModelException(e);
-		}
-	}
+//	@Override
+//	public void move(Worm worm, int nbSteps) throws ModelException {
+//		try {
+//			worm.Move(nbSteps);
+//		} catch (InvalidLocationException e) {
+//			throw new ModelException(e);
+//		}
+//	}
 
 	@Override
 	public void turn(Worm worm, double angle) throws ModelException {
@@ -44,19 +53,19 @@ public class Facade implements IFacade {
 
 	}
 
-	@Override
-	public void jump(Worm worm) throws ModelException {
-		try {
-			worm.Jump();
-		} catch (InvalidLocationException e) {
-			throw new ModelException(e);
-		}
-	}
+//	@Override
+//	public void jump(Worm worm) throws ModelException {
+//		try {
+//			worm.Jump();
+//		} catch (InvalidLocationException e) {
+//			throw new ModelException(e);
+//		}
+//	}
 
-	@Override
-	public double getJumpTime(Worm worm) throws ModelException {
-		return worm.getJumpTime();
-	}
+//	@Override
+//	public double getJumpTime(Worm worm) throws ModelException {
+//		return worm.getJumpTime();
+//	}
 
 	@Override
 	public double[] getJumpStep(Worm worm, double t) throws ModelException {
@@ -67,15 +76,15 @@ public class Facade implements IFacade {
 		}
 	}
 
-	@Override
-	public double getX(Worm worm) throws ModelException {
-		return worm.getLocation()[0];
-	}
+//	@Override
+//	public double getX(Worm worm) throws ModelException {
+//		return worm.getLocation()[0];
+//	}
 
-	@Override
-	public double getY(Worm worm) throws ModelException {
-		return worm.getLocation()[1];
-	}
+//	@Override
+//	public double getY(Worm worm) throws ModelException {
+//		return worm.getLocation()[1];
+//	}
 
 	@Override
 	public double getOrientation(Worm worm) throws ModelException {
@@ -128,6 +137,260 @@ public class Facade implements IFacade {
 	@Override
 	public double getMass(Worm worm) throws ModelException {
 		return worm.getMass();
+	}
+
+	@Override
+	public World createWorld(double width, double height, boolean[][] passableMap) throws ModelException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void terminate(World world) throws ModelException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isTerminated(World world) throws ModelException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double getWorldWidth(World world) throws ModelException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getWorldHeight(World world) throws ModelException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isPassable(World world, double[] location) throws ModelException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isPassable(World world, double[] center, double radius) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAdjacent(World world, double[] center, double radius) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean hasAsWorm(World world, Worm worm) throws ModelException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void addWorm(World world, Worm worm) throws ModelException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeWorm(World world, Worm worm) throws ModelException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Worm> getAllWorms(World world) throws ModelException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasAsFood(World world, Food food) throws ModelException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void addFood(World world, Food food) throws ModelException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeFood(World world, Food food) throws ModelException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Collection<Object> getAllItems(World world) throws ModelException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Team> getAllTeams(World world) throws ModelException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasActiveGame(World world) throws ModelException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Worm getActiveWorm(World world) throws ModelException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void startGame(World world) throws ModelException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void finishGame(World world) throws ModelException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void activateNextWorm(World world) throws ModelException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getWinner(World world) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Worm createWorm(World world, double[] location, double direction, double radius, String name, Team team)
+			throws ModelException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void terminate(Worm worm) throws ModelException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isTerminated(Worm worm) throws ModelException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double[] getLocation(Worm worm) throws ModelException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BigInteger getNbHitPoints(Worm worm) throws ModelException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void incrementNbHitPoints(Worm worm, long delta) throws ModelException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public World getWorld(Worm worm) throws ModelException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double[] getFurthestLocationInDirection(Worm worm, double direction, double maxDistance)
+			throws ModelException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void move(Worm worm) throws ModelException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double getJumpTime(Worm worm, double deltaT) throws ModelException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void jump(Worm worm, double timeStep) throws ModelException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Food createFood(World world, double[] location) throws ModelException {
+		Food f;
+		try {
+			f = new Food(location, WorldConstants.getFoodRadius());
+		} catch (Exception e) {
+			throw new ModelException(e);
+		}
+
+		return null;
+	}
+
+	@Override
+	public void terminate(Food food) throws ModelException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isTerminated(Food food) throws ModelException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double[] getLocation(Food food) throws ModelException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getRadius(Food food) throws ModelException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getMass(Food food) throws ModelException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public World getWorld(Food food) throws ModelException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- OGP1718-Worms/src/worms/model/Worm.java
+++ OGP1718-Worms/src/worms/model/Worm.java
@@ -25,7 +25,7 @@ import worms.exceptions.InvalidRadiusException;
  * @Invar worm's jump speed (vector)magnitude 
  * 		| isValidJumpSpeedMagnitude(getJumpSpeedMagnitude())
  */
-public class Worm {
+public class Worm extends GameObject{
 
 	/**
 	 * The constructor for creating a new worm. The worm have it's properties initialized based on:
@@ -44,12 +44,8 @@ public class Worm {
 	 * @param name
 	 *      The given name to try to assign to the worm.
 	 *      
-	 * @post The location of the worm is the same as the given location.
-	 * 		|new.getLocation() == location
 	 * @post The direction of the worm is the same as the given direction.
 	 * 		|new.getDirection() == direction
-	 * @post The radius of the worm is the same as the given radius.
-	 * 		|new.getRadius() == radius
 	 * @post The name of the worm is the same as the given name.
 	 * 		|new.getName() == name
 	 * 	
@@ -69,77 +65,13 @@ public class Worm {
 	 */
 	public Worm(double[] location, double direction, double radius, String name)
 			throws InvalidWormNameException, InvalidRadiusException, InvalidLocationException {
-		this.setLocation(location);
+		super(location, radius);
 		this.setDirection(direction);
-		this.setRadius(radius);
 		this.setName(name);
 		this.resetActionPoints();
 	}
 
-	/**
-	 * Sets the location of a worm. If the given location is invalid for any worm,
-	 * the location doesn't exist (null) or contains invalid coordinates, any coordinate being
-	 * NaN (Not a Number), the location will be rejected and a InvalidLocationException will be thrown.
-	 * 
-	 * @param location
-	 * 		A given tuple of coordinates, with at index 0 the supposed x-coordinate and at index 1 the supposed
-	 * 		y - coordinate.
-	 * 
-	 * @post Location of the worm should be the given location. 
-	 * 		|new.getLocation() == location
-	 * 
-	 * @throws InvalidLocationException
-	 *         Given location doesn't resemble a valid location.
-	 *      |!isValidlocation(location)
-	 * 
-	 */
-	@Raw
-	public void setLocation(double[] location) throws InvalidLocationException {
-		if (!isValidLocation(location)) {
-			throw new InvalidLocationException(location);
-		}
-		
-		this.location[0] = location[0]; // worms location gets given location
-		this.location[1] = location[1];
-	}
 
-	/**
-	 * Checks whether a given location exists and only contains valid coordinates.
-	 * 
-	 * @param location
-	 * 		A given set of coordinates, with at index 0 the supposed x-location and at index 1
-	 * 		the supposed y-location.
-	 * @return True if the location is valid(not null) and both indexes are within
-	 *         the given bounds. 
-	 *         |result == 
-	 *         | (location != null)
-	 *         | && (location[0] !=Double.NaN && location[1] != Double.NaN)
-	 */
-	public static boolean isValidLocation(double[] location) {
-		if (location == null) {
-			return false;
-		}
-		if (location[0] == Double.NaN || location[1] == Double.NaN) {
-			return false;
-		}
-		
-		return true;
-	}
-
-	/**
-	* A double is a primitive type in Java, this means a double is initialized at 0.0 (it's default value)
-	* Upon declaration of this array (tuple) it's contents will be a 0.0 at index 0 and 0.0 at index 1.
-	* Therefore, the initialized state of 'location' will be a valid position for the class's invariant
-	*/
-	private double[] location = new double[2];
-
-	/**
-	 * returns the worm's current x and y location.
-	 */
-	@Basic @Raw
-	public double[] getLocation() {
-		return this.location;
-	}
 
 	/**
 	 * Sets the direction that the worm should be facing. Direction is presented in
@@ -174,104 +106,6 @@ public class Worm {
 	}
 
 	/**
-	 * Returns the effective radius of the current worm.
-	 */
-	@Basic
-	public double getRadius() {
-		return this.radius;
-	}
-
-	/**
-	 * Sets the radius of a worm. If the radius is invalid, meaning it's smaller than the minimum
-	 * allowed radius of a worm, an InvalidRadiusException will be thrown.
-	 * 
-	 * @param radius
-	 *        The given radius for a worm, bigger radius is bigger worm. Minimum
-	 *        radius may be in effect.
-	 * @post Sets the worm its radius to the given radius.
-	 * 		|new.getRadius() == radius
-	 * @effect This function also changes the mass and thus max Action Points, since
-	 *         a worm's weight depends on it's size.
-	 *     | setMass()
-	 * @throws InvalidRadiusException
-	 *         Whenever the radius is out of bounds.
-	 *     |!isValidRadius(radius)
-	 *     
-	 * @note We currently do not change a worm's current AP when we change it's radius (and
-	 * 	therefore it's mass). This can be easily implemented later however.
-	 */
-	public void setRadius(double radius) throws InvalidRadiusException {
-		if (!isValidRadius(radius)) {
-			throw new InvalidRadiusException(radius);
-		}
-
-		this.radius = radius;
-		setMass();
-	}
-	
-	/**
-	 * We introduce a static final to set the minimum lower bounds where 'every' worm should comply to.
-	 */
-	private final static double MINIMUM_RADIUS = 0.25;
-	
-	/**
-	* The default initialization would set the radius to 0.0, this however isn't correct
-	* according to the rules of the class invariant stating it should be at least equal or bigger
-	* to the minimum radius.
-	*/
-	private double radius = 0.25;
-	
-
-
-	/**
-	 * Checks whether a given radius is valid.
-	 * 
-	 * @param radius
-	 *      The given radius for a worm, bigger radius is bigger worm. Minimum
-	 *      radius may be in effect.
-	 * @return True if the given radius is equal to or bigger than the minimum
-	 *         radius. 
-	 *      | result == 
-	 *      | minRadius <= radius
-	 * @return False if the given radius is smaller than the minimum radius. 
-	 * 		|result == 
-	 * 		| minRadius > radius
-	 */
-	public boolean isValidRadius(double radius) {
-		if (MINIMUM_RADIUS > radius) {
-			return false;
-		}
-
-		return MINIMUM_RADIUS <= radius;
-	}
-
-	/**
-	 * A given constant value, therefore a static final value.
-	 */
-	private static final double WORM_DENSITY = 1062;
-
-	/**
-	 * Calculates and returns the resulting mass of a worm with it's properties
-	 * (radius for example).
-	 * 
-	 * @return calculateMass returns the worms effective mass, calculated using its
-	 *       own radius and density.
-	 *       |result == 
-	 *       |		worm_density * (((double)4 /(double)3) * Math.PI * Math.pow(this.radius, 3))
-	 */
-	public double calculateMass() {
-		return WORM_DENSITY * (((double) 4 / (double) 3) * Math.PI * Math.pow(this.radius, 3));
-	}
-
-	/**
-	 * Returns the mass for the given worm.
-	 */
-	@Basic
-	public double getMass() {
-		return this.mass;
-	}
-
-	/**
 	 * This function sets the theoretical mass of a worm. Since a worm's maximum
 	 * Action Points depends on a worm's weight the worm's max AP will be calculated
 	 * and set in this function as well.
@@ -286,17 +120,12 @@ public class Worm {
 	 * 		Weight and thus max AP only changes whenever the radius changes. However the radius isn't changed
 	 * 		inside this method, thus a worm's radius can be considered constant in and exactly in this function.
 	 */
-	public void setMass() {
-		this.mass = calculateMass();
+	@Override
+	public void generateMass() {
+		this.setMass(calculateMass(WorldConstants.getWormDensity()));
 		this.maxActionPoints = this.calculateMaxActionPoints();
 	}
 
-	/**
-	 * The default initialization for mass would be 0.0, this however isn't correct according
-	 * to our class invariant. We therefore set the initial mass of a worm to be equal to the minimum
-	 * size a worm can be (since a worm's radius is initialized to it's minimum lower bound).
-	 */
-	private double mass =  WORM_DENSITY * (((double) 4 / (double) 3) * Math.PI * Math.pow(MINIMUM_RADIUS, 3));
 
 	/**
 	 * Calculates the maximum amount of action points a worm can have at a certain moment with a certain size (radius).
@@ -869,5 +698,13 @@ public class Worm {
 	private double jumpTime = 0;
 
 
+	@Override
+	public boolean isValidRadius(double radius) {
+		if (WorldConstants.getWormMinimumRadius() > radius) {
+			return false;
+		}
+
+		return WorldConstants.getWormMinimumRadius() <= radius;
+	}
 
 }
