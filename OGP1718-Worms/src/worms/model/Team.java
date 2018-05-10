574ca27227ed01901cdf5fd22a69fec601801725
Bernd Weckx <bernd.weckx@gmail.com>
2018-05-10 20:21:09 +0200

    stuff

--- OGP1718-Worms/src/worms/model/Team.java
+++ OGP1718-Worms/src/worms/model/Team.java
@@ -269,31 +269,50 @@ public class Team {
 			throw new IllegalArgumentException("Team to be merged is null");
 		}
 		if(team.equals(this) == false) {
-			for (Worm worm : team.teamRoster) {
-				teamRosterCopy.add(worm);
-				
+			teamRosterCopy = new ArrayList<Worm>(team.getAlphabeticalListTeamRoster());	
+			if(!teamRosterCopy.isEmpty()) {
+				Team fromTeam = teamRosterCopy.get(0).getTeam();
+				try {
+					fromTeam.removeWorm(teamRosterCopy.toArray(new Worm[teamRosterCopy.size()]));
+					for (Worm worm : teamRosterCopy) {
+						worm.setTeam(null);
+					}
+					this.addWorm(teamRosterCopy.toArray(new Worm[teamRosterCopy.size()]));
+					
+				} catch (Exception e) {
+					for (Worm worm : teamRosterCopy) {
+						team.addWorm(teamRosterCopy.toArray(new Worm[teamRosterCopy.size()]));
+						worm.setTeam(fromTeam); //FIx original team
+						throw e;
+					}
 				}
-			for (Worm w : teamRosterCopy) {
-				if(w == null) {
-					return;
-				}
-				if(w.isTerminated()) {
-					return;
-				}
+
 			}
 			
-			for (Worm worm : teamRosterCopy) {
-				team.removeWorm(worm);
-				try {
-					this.addWorm(worm);	
-				} catch (Exception e) {
-					for(Worm w : teamRosterCopy) {
-						this.removeWorm(w);
-						team.addWorm(w);
-					}
-					return;
-				}	
-			}
+//			for (Worm worm : team.teamRoster) {
+//				teamRosterCopy.add(worm);
+//			}
+//			for (Worm w : teamRosterCopy) {
+//				if(w == null) {
+//					return;
+//				}
+//				if(w.isTerminated()) {
+//					return;
+//				}
+//			}
+//			
+//			for (Worm worm : teamRosterCopy) {
+//				team.removeWorm(worm);
+//				try {
+//					this.addWorm(worm);	
+//				} catch (Exception e) {
+//					for(Worm w : teamRosterCopy) {
+//						this.removeWorm(w);
+//						team.addWorm(w);
+//					}
+//					return;
+//				}	
+//			}
 		}
 		if (team.equals(this) != false) {
 			throw new IllegalArgumentException("Both teams are the same team. Merging won't be able to.");
